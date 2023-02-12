d9ce977603dfdd31bb8ba30fa1617a7552377ffa
w-garden <w.garden316@gmail.com>
2023-02-12 23:39:16 +0900

    test

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,4 +1,13 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Order {
+    @Id @GeneratedValue
+    private Long id;
+
+    private Long memberId;
 }
