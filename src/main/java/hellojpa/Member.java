ced5689098cbad4bd635de0ea30d886c4fe3e640
w-garden <w.garden316@gmail.com>
2023-02-13 22:37:31 +0900

    Initial commit

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,23 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    @Column(name = "USERNAME")
+    private String name;
+    
+    @Column(name = "TEAM_ID")
+    private Long teamId;
+
+}
