75e68011fc3c66aee4776f1fe06ac992591fc606
w-garden <w.garden316@gmail.com>
2023-02-13 22:20:12 +0900

    20230213 테이블설계에 맞추어 엔티티 설계

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,16 +3,14 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="MEMBER_ID")
     private Long id;
     private String name;
     private String city;
