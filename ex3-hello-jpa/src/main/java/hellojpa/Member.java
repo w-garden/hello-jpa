bc3abac1f41ebfb7764c90a99769930bc10d50f9
w-garden <w.garden316@gmail.com>
2023-02-26 23:22:01 +0900

    JPQL 예제 프로젝트 생성

---
+++ ex3-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,39 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * 다대일 매핑관계
+ */
+@Getter
+@Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+
+    //연관관계 없이 매핑
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+//    @ManyToOne //Member입장에서 Many, Team입장에서 one
+//    @JoinColumn(name="TEAM_ID")
+//    private Team team01;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Team team;
+
+
+    @Column(name = "USERNAME")
+    private String username;
+
+//    public void changeTeam(Team team01) { //연관관계 편의 메서드
+//        this.team01=team01;
+//        team01.getMembers().add(this);
+//    }
+
+}
---
+++ ex3-hello-jpa/src/main/java/hellojpa/Team.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name="TEAM_ID")
+    private Long id;
+    private String name;
+
+//    @OneToMany(mappedBy = "team") //Team에서 Member로는 1:다
+//    List<Member> members = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name="TEAM_ID") //추가안하면 JoinTable 전략으로 진행됨
+    List<Member> members = new ArrayList<>();
+
+
+
+}
