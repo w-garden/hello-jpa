b28e4e70f0a099cf8319828a1d97eca656205226
w-garden <w.garden316@gmail.com>
2023-03-01 21:28:49 +0900

    20230301 JPASTORE 프로젝트 생성

---
+++ jpastroe/src/test/java/jpabook/jpastroe/JpastroeApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpastroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpastroeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
