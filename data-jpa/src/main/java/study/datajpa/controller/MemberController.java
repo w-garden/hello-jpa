354952e04faac3c9f700c8434b99563259188b91
w-garden <w.garden316@gmail.com>
2023-03-08 23:14:20 +0900

    Persistable 구현

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -42,7 +42,7 @@ public class MemberController {
         return memberRepository.findAll(pageable).map(MemberDto::new);
     }
 
-    @PostConstruct
+//    @PostConstruct
     public void init(){
         for(int i=0;i<100;i++){
             memberRepository.save(new Member("user"+i,i,null));
---
+++ data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,35 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Item implements Persistable<String> {
+    @Id
+    private String id;
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate==null;
+    }
+}
