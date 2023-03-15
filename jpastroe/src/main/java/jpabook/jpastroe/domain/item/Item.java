5e2b84187e7d7b96affaa8081106fccc45d1f8f3
w-garden <w.garden316@gmail.com>
2023-03-15 22:40:40 +0900

    상품 엔티티 개발

--- jpastroe/src/main/java/jpabook/jpastroe/domain/item/Item.java
+++ jpastroe/src/main/java/jpabook/jpastroe/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpastroe.domain.item;
 
 
 import jpabook.jpastroe.domain.Category;
+import jpabook.jpastroe.eception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,16 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+    public void removeStock(int quantity){
+        int restStock =this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 }
