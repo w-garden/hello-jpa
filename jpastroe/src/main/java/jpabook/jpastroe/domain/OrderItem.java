85acea961573d9a641f9a15c18be3737f7a3cbcf
w-garden <w.garden316@gmail.com>
2023-03-18 20:12:31 +0900

    주문상품 엔티티, 주문 엔티티 개발

--- jpastroe/src/main/java/jpabook/jpastroe/domain/OrderItem.java
+++ jpastroe/src/main/java/jpabook/jpastroe/domain/OrderItem.java
@@ -23,4 +23,15 @@ public class OrderItem {
     private Order order;
     private int orderPrice;
     private int count;
+
+    //==비즈니스 로직==//
+    /**주문취소*/
+    public void cancel() {
+        getItem().addStock(count);
+    }
+    //==조회 로직 ==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
