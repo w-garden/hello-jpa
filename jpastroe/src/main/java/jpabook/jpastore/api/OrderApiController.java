ffa503968f87a5201b01c0dcd6fca3658d9e71a8
w-garden <w.garden316@gmail.com>
2023-04-02 22:33:39 +0900

    컬렉션 타입 조회 V2, V3

--- jpastroe/src/main/java/jpabook/jpastore/api/OrderApiController.java
+++ jpastroe/src/main/java/jpabook/jpastore/api/OrderApiController.java
@@ -8,18 +8,37 @@ import jpabook.jpastore.repository.OrderRepository;
 import jpabook.jpastore.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * V1. 엔티티 직접 노출
  * - 엔티티가 변하면 API 스펙이 변한다
  * - 트랜잭션 안에서 지연 로딩 필요
  * - 양방향 연관 관계
+ *
+ * V2. 엔티티를 조회헤서 DTO로 변환(fetch join 사용 x)
+ * - 트랜잭션 안에서 지연 로딩 필요
+ *
+ * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용 o)
+ * - 페이징 시에는 N 부분을 포기해야함 (대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 기능)
+ *
+ * V4. JPA에서 DTO에서 바로 조회, 컬렉션 N 조회(1 + N Query)
+ * - 페이징 가능
+ *
+ * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
+ * - 페이징 가능
+ *
+ * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
+ * - 페이징 불가능...
  */
 @RestController
 @RequiredArgsConstructor
@@ -45,9 +64,35 @@ public class OrderApiController {
 
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2() {
-        List<Order> orders = orderRepository.findAll();
-        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o ->new OrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * - ToOne 관계만 우선 모두 페치조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value="offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
         return result;
     }
 
@@ -69,14 +114,14 @@ public class OrderApiController {
             this.orderStatus = order.getStatus();
             this.address = order.getMember().getAddress();
             this.orderItems = order.getOrderItems().stream()
-                    .map(orderItem -> new OrderItemDto(orderItem)).collect(Collectors.toList());
+                    .map(orderItem -> new OrderItemDto(orderItem)).collect(toList());
 
         }
     }
 
     @Data
     static class OrderItemDto{
-        private String itemName; //상품 평
+        private String itemName; //상품 명
         private int orderPrice; //주문 가격
         private int count; //주문 수량
 
