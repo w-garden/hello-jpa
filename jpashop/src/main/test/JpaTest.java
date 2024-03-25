b82f9a663a8c386e4ee2f1217a361099783a6464
w-garden <w.garden316@gmail.com>
2024-03-25 23:07:16 +0900

    JPA 프로젝트 수정

--- jpashop/src/main/test/JpaTest.java
+++ jpashop/src/main/test/JpaTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,7 +17,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 @Transactional
-
 public class JpaTest {
     @PersistenceContext
     private EntityManager em;
@@ -31,17 +29,18 @@ public class JpaTest {
     @Autowired
     private ApplicationContext appContext;
     @Test
-    @Rollback(value = false)
+    public void beanTest(){
+        final String[] beanDefinitionNames = appContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+    }
+    @Test
     public void test() {
-//        final String[] beanDefinitionNames = appContext.getBeanDefinitionNames();
-//        for (String beanDefinitionName : beanDefinitionNames) {
-//            System.out.println(beanDefinitionName);
-//        }
 
-//        Member member = new Member();
-//        member.setId(111L);
-//        member.setName("신호철");
-//        em.persist(member);
+        Member member = new Member();
+        member.setName("신호철");
+        em.persist(member);
         Member member1 = em.find(Member.class, 1L);
         System.out.println(member1.getName());
 
